185b2aebe3153464ea28e874642027e73046f645
Tahara Kazuki <ssp0727.lnc@gmail.com>
2021-12-17 20:03:21 +0900

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,24 @@
-console.info('herro')
+import express, { Express } from 'express'
+import socket, { Server } from 'socket.io'
+
+const PORT = 8000
+
+const app: Express = express()
+
+const server = app.listen(PORT, () => {
+  console.info(`server is listening on port ${PORT}`)
+  console.info(`http://localhost:${PORT}`)
+})
+
+const io = new socket.Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+})
+
+io.on('connection', (socket) => {
+  socket.emit('connection', null)
+  console.info('new user connected')
+  console.info(socket.id)
+})
