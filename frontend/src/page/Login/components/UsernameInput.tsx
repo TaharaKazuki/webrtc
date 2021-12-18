608b9745f680d312a1e7df02bc0f2b81117dc337
Tahara Kazuki <ssp0727.lnc@gmail.com>
2021-12-18 12:21:57 +0900

    update

--- frontend/src/page/Login/components/UsernameInput.tsx
+++ frontend/src/page/Login/components/UsernameInput.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { FC, Dispatch } from 'react'
 
-const UsernameInput = () => {
-  return <div></div>
+interface IUsernameProps {
+  username: string
+  setUsername: Dispatch<React.SetStateAction<string>>
+}
+
+const UsernameInput: FC<IUsernameProps> = ({ username, setUsername }) => {
+  return (
+    <div className="loginPage_input_container">
+      <input
+        placeholder="Enter your name"
+        type="text"
+        value={username}
+        onChange={(event) => {
+          setUsername(event.target.value)
+        }}
+        className="loginPage_input background_main_color text_main_color"
+      />
+    </div>
+  )
 }
 
 export default UsernameInput
